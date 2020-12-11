117259cbc1b4ca474c8b908b53b5568f0ca31ce5
alejandrocordoba <alejandrocordoba@192.168.0.5>
2020-12-11 16:49:10 -0300

    Version V2 Final funcionando

--- catalogo_brasil/src/catalogo_brasil/ReadJSONExample.java
+++ catalogo_brasil/src/catalogo_brasil/ReadJSONExample.java
@@ -14,28 +14,28 @@ import org.json.simple.parser.ParseException;
 public class ReadJSONExample {
 
 	public static void main(String[] args) throws IOException, ParseException {
-	
-		FileReader readerProduto = new FileReader("src/test.json");
-		FileReader readerSemeadura = new FileReader("src/semeadura.json");
-		FileReader readerAgronomicas = new FileReader("src/caracteristicas_agronomicas.json");
-		FileReader readerdoencas = new FileReader("src/comportamiento_doencas.json");
-		FileReader readerPhytophtora = new FileReader("src/phytophtora.json");
-		FileReader readerNematoides = new FileReader("src/nematoides.json");
-		FileReader readerGalha = new FileReader("src/galha.json");
-		FileReader readerpontos_fortes = new FileReader("src/pontos_fortes.json");
-		
-		ContainerFactory containerFactory = new ContainerFactory(){
-	        @Override
-	        public Map createObjectContainer() {
-	            return new LinkedHashMap();
-	        }
 
-	        @Override
-	        public List creatArrayContainer() {
-	            return null;
-	        }
-	    };
-		
+		FileReader readerProduto = new FileReader("src/producto_v1.json");
+		FileReader readerSemeadura = new FileReader("src/test2.json");
+		FileReader readerAgronomicas = new FileReader("src/test3.json");
+		FileReader readerdoencas = new FileReader("src/test5.json");
+		FileReader readerPhytophtora = new FileReader("src/test6.json");
+		FileReader readerNematoides = new FileReader("src/nematoides_v2.json");
+		FileReader readerGalha = new FileReader("src/galha_v2.json");
+		FileReader readerpontos_fortes = new FileReader("src/pontos.json");
+
+		ContainerFactory containerFactory = new ContainerFactory() {
+			@Override
+			public Map createObjectContainer() {
+				return new LinkedHashMap();
+			}
+
+			@Override
+			public List creatArrayContainer() {
+				return null;
+			}
+		};
+
 		JSONParser jsonparser1 = new JSONParser();
 		JSONParser jsonparser2 = new JSONParser();
 		JSONParser jsonparser3 = new JSONParser();
@@ -44,62 +44,59 @@ public class ReadJSONExample {
 		JSONParser jsonparser6 = new JSONParser();
 		JSONParser jsonparser7 = new JSONParser();
 		JSONParser jsonparser8 = new JSONParser();
-		
-		Object prod = jsonparser1.parse(readerProduto,(ContainerFactory) containerFactory);
-		Object semead = jsonparser2.parse(readerSemeadura,(ContainerFactory) containerFactory);
-		Object agronom = jsonparser3.parse(readerAgronomicas,(ContainerFactory) containerFactory);
-		Object pdoencas = jsonparser4.parse(readerdoencas,(ContainerFactory) containerFactory);
-		
-		Object phytophtora = jsonparser5.parse(readerPhytophtora,(ContainerFactory) containerFactory);
-		Object nematoides = jsonparser6.parse(readerNematoides,(ContainerFactory) containerFactory);
-		Object galha = jsonparser7.parse(readerGalha,(ContainerFactory) containerFactory);
-		Object fortes = jsonparser8.parse(readerpontos_fortes,(ContainerFactory) containerFactory);
-		
-		
-		//JSONArray arrayProd = (JSONArray) prod;
+
+		Object prod = jsonparser1.parse(readerProduto, (ContainerFactory) containerFactory);
+		Object semead = jsonparser2.parse(readerSemeadura, (ContainerFactory) containerFactory);
+		Object agronom = jsonparser3.parse(readerAgronomicas, (ContainerFactory) containerFactory);
+		Object pdoencas = jsonparser4.parse(readerdoencas, (ContainerFactory) containerFactory);
+
+		Object phytophtora = jsonparser5.parse(readerPhytophtora, (ContainerFactory) containerFactory);
+		Object nematoides = jsonparser6.parse(readerNematoides, (ContainerFactory) containerFactory);
+		Object galha = jsonparser7.parse(readerGalha, (ContainerFactory) containerFactory);
+		Object fortes = jsonparser8.parse(readerpontos_fortes, (ContainerFactory) containerFactory);
+
+		JSONArray arrayProd = (JSONArray) prod;
 		JSONArray arraySemeadura = (JSONArray) semead;
 		JSONArray arrayAgronomica = (JSONArray) agronom;
 		JSONArray arrayDoencas = (JSONArray) pdoencas;
 		JSONArray arrayPhytophtora = (JSONArray) phytophtora;
 		JSONArray arrayNematoides = (JSONArray) nematoides;
 		JSONArray arrayGalha = (JSONArray) galha;
-		
+
 		JSONArray arrayPFortes = (JSONArray) fortes;
-		
+
 		JSONArray tmp = new JSONArray();
-		
-		/*
-		 * for (int i = 0; i < arrayProd.size()-1; i++) {
-		 * 
-		 * LinkedHashMap produto = (LinkedHashMap) arrayProd.get(i); LinkedHashMap
-		 * semeadura = (LinkedHashMap) arraySemeadura.get(i); LinkedHashMap agronomica =
-		 * (LinkedHashMap) arrayAgronomica.get(i); LinkedHashMap pontosF =
-		 * (LinkedHashMap) arrayPFortes.get(i); LinkedHashMap cDoencas = (LinkedHashMap)
-		 * arrayDoencas.get(i);
-		 * 
-		 * LinkedHashMap lPhytophtora = (LinkedHashMap) arrayPhytophtora.get(i);
-		 * LinkedHashMap lNematoides = (LinkedHashMap) arrayNematoides.get(i);
-		 * LinkedHashMap lGalha = (LinkedHashMap) arrayGalha.get(i);
-		 * 
-		 * lNematoides.put("Galha", lGalha);
-		 * 
-		 * cDoencas.put("Phytophtora", lPhytophtora);
-		 * 
-		 * cDoencas.put("Nematoides", lNematoides);
-		 * 
-		 * 
-		 * produto.put("Época de semeadura", semeadura);
-		 * produto.put("Caracteristicas Agronomicas", agronomica);
-		 * produto.put("Pontos Fortes", pontosF); produto.put("Comportamento doenças",
-		 * cDoencas);
-		 * 
-		 * tmp.add(produto);
-		 * 
-		 * //System.out.println(tmp); }
-		 */		System.out.println(tmp);
-		
+
+		for (int i = 0; i < arrayProd.size() - 1; i++) {
+
+			LinkedHashMap produto = (LinkedHashMap) arrayProd.get(i);
+			LinkedHashMap semeadura = (LinkedHashMap) arraySemeadura.get(i);
+			LinkedHashMap agronomica = (LinkedHashMap) arrayAgronomica.get(i);
+			LinkedHashMap pontosF = (LinkedHashMap) arrayPFortes.get(i);
+			LinkedHashMap cDoencas = (LinkedHashMap) arrayDoencas.get(i);
+
+			LinkedHashMap lPhytophtora = (LinkedHashMap) arrayPhytophtora.get(i);
+			LinkedHashMap lNematoides = (LinkedHashMap) arrayNematoides.get(i);
+			LinkedHashMap lGalha = (LinkedHashMap) arrayGalha.get(i);
+
+			lNematoides.put("Galha", lGalha);
+
+			cDoencas.put("Phytophtora", lPhytophtora);
+
+			cDoencas.put("Nematoides", lNematoides);
+
+			produto.put("Época de semeadura", semeadura);
+			produto.put("Caracteristicas Agronomicas", agronomica);
+			produto.put("Pontos Fortes", pontosF);
+			produto.put("Comportamento doenças", cDoencas);
+
+			tmp.add(produto);
+
+			// System.out.println(tmp); }
+		//	System.out.println(tmp);
+
+		}
+		System.out.println(tmp);
+
 	}
-	
-	
-	
 }
